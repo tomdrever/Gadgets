d75f773bb7eeb1eff82f94fe29e8d07e33ce1772
tomdrever <tomdr1001@outlook.com>
2021-09-20 09:42:26 +0100

    Add placeholder biome icons for mods

--- src/main/java/tishtesh/gadgets/client/gui/GadgetGuiItem.java
+++ src/main/java/tishtesh/gadgets/client/gui/GadgetGuiItem.java
@@ -2,8 +2,6 @@ package tishtesh.gadgets.client.gui;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import tishtesh.gadgets.core.Config;
 
 public abstract class GadgetGuiItem {
